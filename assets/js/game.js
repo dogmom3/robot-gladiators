e31775bda9c2a72a7784675d56978bb3b95dd540
dogmom3 <blair.seivers@yahoo.com>
2022-01-13 17:46:14 -0500

    add javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,7 +25,23 @@ playerHealth = playerHealth - enemyAttack;
     //log a resulting message to the console so we know that it worked.
 console.log(
     enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-);
+); 
+
+//check player's health
+if(playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+}
+else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+}
+
+//check enemy's health
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+    }
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
 };
 
 fight();
